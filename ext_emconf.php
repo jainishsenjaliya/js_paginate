bad51114c9bd46e318802a7169842da52e04513f
jainish.online <jainish.online@gmail.com>
2016-01-12 10:22:56 +0530

    Compatibility with TYPO3 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0 - 6.2.99',
+			'typo3' => '6.0.0 - 7.6.99',
 		),
 		'conflicts' => array(
 		),
